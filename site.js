a90fc13c4872cb807f69a5d1e2cb66d8029b274a
liz <cleanbean@gmail.com>
2025-03-05 09:03:00 -0800

    linking up vue with html and adding temp buttons

---
+++ site.js
@@ -0,0 +1,14 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            message: 'hello',
+            message2: '',
+            show: 'home'
+        }},
+        methods: {
+
+        },
+
+})
+
+app.mount('#app')
