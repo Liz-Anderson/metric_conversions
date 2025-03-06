0177502ef093077e11d86ca5fc6a972b236a73cd
liz <cleanbean@gmail.com>
2025-03-06 15:09:40 -0800

    cleaning up, added more data, started on conversion methods

--- site.js
+++ site.js
@@ -1,13 +1,43 @@
 const app = Vue.createApp({
     data() {
         return {
-            message: 'hello',
-            message2: '',
-            show: 'home'
+            category: 'temperature',
+            inputAmount: 0,
+            outputAmount: 0,
+            inputUnit: '',
+            outputUnit: '',
+            length: {
+                'feet': 0.3048,
+                'mi': 1609.34,
+                'm': 1,
+                'km': 1000,
+                'yd': 0.9144,
+                'in': 0.0254
+            },
+            weight: {
+                'grams': 0,
+                'ounce': 0,
+                'pounds': 0,
+                'kilograms': 0
+            },
+            // figure out units for volume
+            volume: {
+
+            }
+
         }},
         methods: {
+            // create temperature function
+            tempConversion() {
 
+            },
+            lwvConversion() {
+                let startingAmount = this.inputAmount * this.category[this.inputUnit]
+                this.outputAmount = startingAmount/this.category[this.outputUnit]
+            }
         },
+
+        // add functions to computed section?
 
 })
 
