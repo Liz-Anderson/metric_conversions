169158052742d3b179e7abe9f7461d26307b744e
liz <cleanbean@gmail.com>
2025-03-21 17:59:42 -0700

    working on creating input and drop down

--- site.js
+++ site.js
@@ -6,32 +6,41 @@ const app = Vue.createApp({
             outputAmount: 0,
             inputUnit: '',
             outputUnit: '',
-            length: {
-                'ft': 0.3048,
-                'mi': 1609.34,
-                'm': 1,
-                'km': 1000,
-                'yd': 0.9144,
-                'in': 0.0254,
-                'cm': 0.01
-            },
-            weight: {
-                'gram': 0.00220462,
-                'ounce': 0.0625,
-                'pound': 1,
-                'kilogram': 2.20462
-            },
-            // figure out units for volume
-            volume: {
-                'tsp': 0,
-                'Tbsp': 0,
-                'cup': 0,
-                'pint': 0,
-                'quart': 0,
-                'gallon': 0,
-                'ml': 0,
-                'fluid ounce': 0
-            }
+            // change the way this data is organized
+            measurementChoice: [
+                    {length: {
+                        ft: 0.3048,
+                        mi: 1609.34,
+                        'm': 1,
+                        'km': 1000,
+                        'yd': 0.9144,
+                        'in': 0.0254,
+                        'cm': 0.01
+                        },
+                        keys: ['ft', 'mi', 'm', 'km', 'yd', 'in', 'cm']
+                    },
+                    {weight: {
+                        gram: 0.00220462,
+                        'ounce': 0.0625,
+                        'pound': 1,
+                        'kilogram': 2.20462
+                        },
+                        keys: ['gram', 'ounce', 'pound', 'kilogram']
+                    },
+                    // figure out units for volume
+                    {volume: {
+                        'tsp': 0,
+                        'Tbsp': 0,
+                        'cup': 0,
+                        'pint': 0,
+                        'quart': 0,
+                        'gallon': 0,
+                        'ml': 0,
+                        'fluid ounce': 0
+                        },
+                        keys: ['tsp', 'Tbsp', 'cup', 'pint', 'quart', 'gallon', 'ml', 'fluid ounce']
+                    }
+            ]
 
         }},
         methods: {
@@ -44,8 +53,18 @@ const app = Vue.createApp({
                 this.outputAmount = startingAmount/this.category[this.outputUnit]
             },
             test() {
-                console.log(this.category)
-                console.log(Object.keys(`${this.category}`))
+                console.log('1', this.category)
+                for (item of this.measurementChoice) {
+                    
+                    if (Object.keys(item) === 'weight') {
+                        console.log('poop')
+                        console.log('2', Object.keys(item))
+                    }
+                    console.log('2.5', item)
+                    
+                }
+                console.log('3', JSON.parse(JSON.stringify(this.measurementChoice)))
+                console.log('4', Object.keys(`${this.category}`))
             }
         },
         created: function() {
