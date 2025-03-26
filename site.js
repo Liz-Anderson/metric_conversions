e201459c68614bd0f173e1eeb2e4fb6c8cd48205
liz <cleanbean@gmail.com>
2025-03-25 23:17:40 -0700

    added volume measurements, added temperature and length sections

--- site.js
+++ site.js
@@ -1,11 +1,15 @@
 const app = Vue.createApp({
     data() {
         return {
-            category: 'poop',
+            category: 'weight',
             inputAmount: 0,
             outputAmount: 0,
             inputUnit: '',
             outputUnit: '',
+            temperature: {
+                C: 0,
+                F: 32
+            },
             length: {
                 ft: 0.3048,
                 mi: 1609.34,
@@ -14,28 +18,24 @@ const app = Vue.createApp({
                 'yd': 0.9144,
                 'in': 0.0254,
                 'cm': 0.01,
-                keys: ['ft', 'mi', 'm', 'km', 'yd', 'in', 'cm']
             },
-                
-            
             weight: {
                 gram: 0.00220462,
                 'ounce': 0.0625,
                 'pound': 1,
                 'kilogram': 2.20462,
-                keys: ['gram', 'ounce', 'pound', 'kilogram']
             },
-
             volume: {
-                'tsp': 0,
-                'Tbsp': 0,
-                'cup': 0,
-                'pint': 0,
-                'quart': 0,
-                'gallon': 0,
-                'ml': 0,
-                'fluid ounce': 0,
-                keys: ['tsp', 'Tbsp', 'cup', 'pint', 'quart', 'gallon', 'ml', 'fluid ounce']
+                'tsp': 0.0208333,
+                'Tbsp': 0.0625,
+                'cup': 1,
+                'pint': 2,
+                'quart': 4,
+                'gallon': 16,
+                'ml': 0.00422675,
+                'liter': 4.22675,
+                'US fluid ounce': 0.125,
+                'Imperial fluid ounce': 0.120095,
             },
                 
                     
@@ -90,7 +90,6 @@ const app = Vue.createApp({
                 // for (item of this.measurementChoice) {
                     
                 //     if (Object.keys(item) === 'weight') {
-                //         console.log('poop')
                 //         console.log('2', Object.keys(item))
                 //     }
                 //     console.log('2.5', item)
